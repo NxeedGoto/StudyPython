2ebb8cfd713a7d3370eb635756de57b2c6e65ccd
Nxeed <645520409@qq.com>
2020-04-21 15:02:34 +0800

    新添加了表格操作的类

---
+++ 爬虫学习/工具目录/ExcelOperation.py
@@ -0,0 +1,32 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
+
+class ExcelOperation(object):
+    def __init__(self, name):
+        self.excel_name = name
+        self.excel_workbook = self.load_excel()
+
+    def __del__(self):
+        try:
+            self.excel_workbook.save(self.excel_name)
+        except Exception as e:
+            print(e)
+
+    def load_excel(self):
+        try:
+            wb = load_workbook(self.excel_name)
+            return wb
+        except FileNotFoundError as e:
+            wb = Workbook()  # 创建一个工作簿
+            return wb
+
+    def create_sheet(self, sheet_name):
+        self.excel_workbook.create_sheet(sheet_name)  # 创建一个工作表
+
+    def write_in_cell(self, row, col, value):
+        ws = self.excel_workbook.active
+        ws.cell(row, col, value)
+
+
+
